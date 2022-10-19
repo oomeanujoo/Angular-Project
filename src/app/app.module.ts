4e0eea7b5e2d576e25633b991f912e73de669318
oomeanujoo <anuzz8602@gmail.com>
2022-10-19 15:52:40 +0530

    just erased default code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RandomComponent } from './random/random.component';
+import { TestModule } from './test/test.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RandomComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TestModule
   ],
   providers: [],
   bootstrap: [AppComponent]
